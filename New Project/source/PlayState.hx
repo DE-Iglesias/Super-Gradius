ead34500cfd0c2bf319d312c26c801581d85bd9c
Unknown <damian.e.iglesias@gmail.com>
2017-09-27 15:54:08 -0300

    Inicio

---
+++ New Project/source/PlayState.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+		
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
